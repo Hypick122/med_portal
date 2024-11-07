04a33ebc9fc04530122e0a702c7f3547d805b54a
sqw1sh <zebraalaalaa@gmail.com>
2024-11-07 16:24:49 +0300

    valid

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-    
+    const login = document.getElementById('login');
+    const password = document.getElementById('password');
+    const submit = document.getElementById('submit');
+
+    if(password) {
+        password.oninput = function() {
+            if(submit) {
+                if(password.value.match(/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/)) {
+                    submit.setAttribute('disabled', '');
+                } else {
+                    submit.setAttribute('disabled', 'disabled');
+                }
+            }
+        }
+    }
 }
